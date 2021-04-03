b83ec4fa1e5d7cd1a31a4c241e2f818cb683163a
ChaimaBenmakhlouf <chaima.benmakhlouf@supinternet.fr>
2021-04-03 19:16:25 +0200

    json decoding update

--- App/Controller/TournamentController.php
+++ App/Controller/TournamentController.php
@@ -7,7 +7,8 @@ use App\Model\TournamentModel;
 
 use App\Model\TeamModel;
 use Framework\Controller;
-
+require('Team.php');
+require('Tournament.php');
 
 class TournamentController extends Controller{
 
@@ -24,12 +25,34 @@ class TournamentController extends Controller{
     public function showClassements() {
         $tournamentModel = new TournamentModel();
         $classements = $tournamentModel->classements();
+        $tournaments = []; 
+        foreach ($classements as $classement) {
+            
+            $teams = [];
+
+            $decodedJson = json_decode($classement['data'], true);
+            if (isset($decodedJson)) {
+                foreach ($decodedJson["teams"] as $teamJson) {
+                    $team = new Team($teamJson["team"], $teamJson["score"]);
+                    array_push($teams, $team);
+                }
+                $tournament = new Tournament($decodedJson["status"], $teams, $classement['tournament_id']);
+                array_push($tournaments, $tournament);
+            }
+        }
+       
+        var_dump($tournaments[0]);
+
+
+
         $this->renderTemplate('classement-list.html.twig', [
             'classements' => $classements
             
             ]);
-            return;
+            
+            
     }
+   
 
     public function createTournament(){
         $tournament = new TournamentModel();
